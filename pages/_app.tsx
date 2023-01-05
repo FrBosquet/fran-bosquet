614ccfbe9f125e80b313182481c758990cb518fc
Fran Bosquet <frbosquet@gmail.com>
2023-01-05 19:28:02 +0100

    add tailwind

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,15 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { Poppins } from '@next/font/google';
+import type { AppProps } from 'next/app';
+import '../styles/globals.css';
+
+const poppins = Poppins({
+  weight: ['100', '300', '700'],
+  variable: '--font-poppins',
+  subsets: ["latin"]
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <main className={`${poppins.variable} font-sans`}>
+    <Component {...pageProps} />
+  </main>
 }
