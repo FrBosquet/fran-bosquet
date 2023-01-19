96956a8f06f9fe41f136ff9e33b0d749484ebad0
Fran Bosquet <frbosquet@gmail.com>
2023-01-19 18:56:27 +0100

    add social icons

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import { GithubIcon, InstagramIcon, TwitterIcon } from '../components/icons'
 
 export default function Home() {
   return (
@@ -13,6 +14,11 @@ export default function Home() {
         <header className='p-4 flex items-center gap-2'>
           <h1 className='text-2xl'>Fran<span className='text-violet-400'>Bosquet</span></h1>
           <p className='text-gray-500 text-sm hidden md:block'>Mi blog personal</p>
+          <menu className='flex-1 flex items-center gap-2 justify-end'>
+            <a href="https://github.com/FrBosquet" target="_blank" rel="noreferrer"><GithubIcon className="w-5 hover:text-violet-400 transition-all" /></a>
+            <a href="https://twitter.com/FrBosquet" target="_blank" rel="noreferrer"><TwitterIcon className="w-5 hover:text-violet-400 transition-all" /></a>
+            <a href="https://instagram.com/frbosquet" target="_blank" rel="noreferrer"><InstagramIcon className="w-5 hover:text-violet-400 transition-all" /></a>
+          </menu>
         </header>
       </section>
     </>
