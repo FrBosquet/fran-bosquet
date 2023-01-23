c72f2f3e24af7e6cbb0d16e36e71199face9b3c4
Fran Bosquet <frbosquet@gmail.com>
2023-01-23 19:10:07 +0100

    add go back link

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import { MDXProvider } from '@mdx-js/react';
 import { Poppins } from '@next/font/google';
 import type { AppProps } from 'next/app';
+import Link from 'next/link';
 import { GithubIcon, InstagramIcon, TwitterIcon } from '../components/icons';
 import { mdxComponents } from '../components/mdxComponents';
 import '../styles/globals.css';
@@ -16,7 +17,9 @@ export default function App({ Component, pageProps }: AppProps) {
     <main className={`${poppins.variable} font-sans`}>
       <section className="bg-gradient-to-b from-gray-800 to-gray-700 min-h-screen text-gray-200">
         <header className='p-4 flex items-center gap-2'>
-          <h1 className='text-2xl'>Fran<span className='text-violet-400'>Bosquet</span></h1>
+          <Link href="/">
+            <h1 className='text-2xl'>Fran<span className='text-violet-400'>Bosquet</span></h1>
+          </Link>
           <p className='text-gray-500 text-sm hidden md:block'>Mi blog personal</p>
           <menu className='flex-1 flex items-center gap-2 justify-end'>
             <a href="https://github.com/FrBosquet" target="_blank" rel="noreferrer"><GithubIcon className="w-5 hover:text-violet-400 transition-all" /></a>
