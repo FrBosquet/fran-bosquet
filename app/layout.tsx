ee8012672171f331fbabd403327e04de869bbf80
Fran Bosquet <frbosquet@gmail.com>
2023-08-25 12:56:01 +0200

    add tag filtering

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,9 +19,9 @@ export default function Layout({ children }: {
     <html lang="es">
       <body className="text-gray-200">
         <main className={`${poppins.variable} font-sans`}>
-          <section className="min-h-screen">
+          <section className="min-h-screen flex flex-col">
             <Header />
-            <article className=' bg-gradient-to-b from-gray-900 to-gray-800'>
+            <article className='flex-1 bg-gradient-to-b from-gray-900 to-gray-800'>
               <div className='
                 max-w-7xl mx-auto
                 p-4 lg:px-8 pb-4
--- components/Tag.tsx
+++ components/Tag.tsx
@@ -1,5 +1,8 @@
+import Link from "next/link"
 import { ReactNode } from "react"
 
 export const Tag = ({ children }: { children: ReactNode }) => {
-	return <div className="px-2 h-6 flex items-center bg-gradient-to-br text-gray-800 from-teal-100 to-orange-100 text-xs">{children}</div>
+	return <Link href={`/${children}`}>
+		<div className="px-2 h-6 flex items-center transition-all bg-gradient-to-br hover:to-orange-300 text-gray-800 from-teal-100 to-orange-100 text-xs">{children}</div>
+	</Link>
 }
