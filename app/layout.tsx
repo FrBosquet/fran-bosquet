a4751c3a0b49e299a08f5b0166016b46ebefd3d6
Fran Bosquet <frbosquet@gmail.com>
2023-08-25 12:39:07 +0200

    fix layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -21,8 +21,13 @@ export default function Layout({ children }: {
         <main className={`${poppins.variable} font-sans`}>
           <section className="min-h-screen">
             <Header />
-            <article className='mx-auto p-4 lg:px-8 pb-4 bg-gradient-to-b from-gray-900 to-gray-800'>
-              {children}
+            <article className=' bg-gradient-to-b from-gray-900 to-gray-800'>
+              <div className='
+                max-w-7xl mx-auto
+                p-4 lg:px-8 pb-4
+              '>
+                {children}
+              </div>
               <Analytics />
             </article>
           </section>
