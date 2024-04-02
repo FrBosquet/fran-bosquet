94c277a8640205bd5a4083586d4319a1a2173ae0
FrBosquet <frbosquet@gmail.com>
2024-04-02 18:40:47 +0200

    fix

--- app/[tag]/page.tsx
+++ app/[tag]/page.tsx
@@ -8,8 +8,8 @@ export default async function Page({ params: { tag } }: { params: { tag: string 
   return <>
     <h1 className='text-xl pb-6'>Post que contienen el tag <pre className="inline text-teal-300 hover:text-orange-400">{tag}<Link href='/'>(x)</Link></pre></h1>
     <section className='grid gap-4 lg:gap-8 xl:gap-12 grid-cols-1 lg:grid-cols-2 xl:grid-cols-3'>
-      {posts.map((post, index) => {
-        return <PostLink post={post} key={post.slug} featured={index === 0} />
+      {posts.map((post) => {
+        return <PostLink post={post} key={post.slug} />
       })}
     </section>
   </>
