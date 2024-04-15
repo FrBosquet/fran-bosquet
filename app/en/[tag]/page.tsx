35343747c18dab623579c0645e66a0bc65dc2b90
FrBosquet <21219312+FrBosquet@users.noreply.github.com>
2024-04-15 20:33:57 +0200

    more intl fixes

--- app/en/[tag]/page.tsx
+++ app/en/[tag]/page.tsx
@@ -6,7 +6,7 @@ export default async function Page({ params: { tag } }: { params: { tag: string 
   const posts = await getPosts(Lang.EN, tag)
 
   return <>
-    <h1 className='text-xl pb-6'>Post containing tag <pre className="inline text-teal-300 hover:text-orange-400">{tag}<Link href='/'>(x)</Link></pre></h1>
+    <h1 className='text-xl pb-6'>Post containing tag <pre className="inline text-teal-300 hover:text-orange-400">{tag}<Link href='/en'>(x)</Link></pre></h1>
     <section className='grid gap-4 lg:gap-8 xl:gap-12 grid-cols-1 lg:grid-cols-2 xl:grid-cols-3'>
       {posts.map((post) => {
         return <PostLink post={post} key={post.slug} lang={Lang.EN} />
