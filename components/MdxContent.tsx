4726f6f7b06ecaad0cd7d523482b48833142e5d7
Fran Bosquet <21219312+FrBosquet@users.noreply.github.com>
2024-04-19 11:44:50 +0200

    make font small in mobile

--- components/MdxContent.tsx
+++ components/MdxContent.tsx
@@ -40,7 +40,7 @@ const mdxComponents = {
   h1: (props: HTMLAttributes<HTMLHeadingElement>) => <h1 className='text-4xl bg-gradient-to-r from-teal-200 to-orange-200 bg-clip-text text-transparent mt-8 mb-4'>{props.children}</h1>,
   h2: (props: HTMLAttributes<HTMLHeadingElement>) => <h2 className='text-3xl font-medium text-orange-300 mt-3'>{props.children}</h2>,
   h3: (props: HTMLAttributes<HTMLHeadingElement>) => <h3 className='text-2xl text-orange-200 mb-1 '>{props.children}</h3>,
-  p: (props: HTMLAttributes<HTMLParagraphElement>) => <p className='text-lg font-ligth pb-3 leading-8 tracking-[0.1rem]'>{props.children}</p>,
+  p: (props: HTMLAttributes<HTMLParagraphElement>) => <p className='text-sm mg:text-lg font-ligth pb-3 leading-8 tracking-[0.1rem]'>{props.children}</p>,
   img: (props: any) => <img className="w-auto mb-1 m-auto" title={props.alt} alt={props.alt} src={props.src} />,
   blockquote: (props: HTMLAttributes<HTMLQuoteElement>) => <blockquote className="p-3 bg-gray-950 rounded-lg mb-4 shadow-md text-lg text-slate-100 leading-8 tracking-wide">{props.children}</blockquote>,
   ul: (props: HTMLAttributes<HTMLUListElement>) => <ul className="text-lg mb-4">{props.children}</ul>,
