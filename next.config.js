5f6ddc6855cca81d6188d5eefe794d7b12261c31
FrBosquet <21219312+FrBosquet@users.noreply.github.com>
2024-05-02 13:31:11 +0200

    add image processing dev plugin

--- next.config.js
+++ next.config.js
@@ -1,9 +1,29 @@
+const chokidar = require('chokidar')
+const { exec } = require('child_process')
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   pageExtensions: ['tsx', 'mdx'],
   experimental: {
     mdxRs: true,
+  },
+  webpack: (config, { dev, isServer, nextRuntime }) => {
+    if (dev && isServer && nextRuntime === 'nodejs') {
+      console.log('Running in dev mode. Watching folders')
+
+      config.plugins.push(
+        (nextConfig) => {
+          chokidar.watch('public/images/*.(jpg|JPG|jpeg|JPEG|png|PNG)', { persistent: true }).on('add', () => {
+            exec('pnpm images')
+          })
+
+          return nextConfig
+        }
+      )
+    }
+
+    return config
   },
   async rewrites() {
     return {
