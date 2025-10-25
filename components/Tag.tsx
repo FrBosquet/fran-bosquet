4e5c27cf4c376d587e407190d4e1fae4f8ea5858
FrBosquet@MBA <21219312+FrBosquet@users.noreply.github.com>
2025-10-25 18:10:25 +0200

    Nest tag routes

--- components/Tag.tsx
+++ components/Tag.tsx
@@ -5,7 +5,7 @@ export const Tag = ({ children, lang }: { children: string; lang: Lang }) => {
   return (
     <Link
       className="flex h-6 items-center bg-gradient-to-br from-teal-100 to-orange-100 px-2 text-xs text-gray-800 transition-all hover:to-orange-300"
-      href={`${lang === 'en' ? '/en' : ''}/${children}`}
+      href={`${lang === 'en' ? '/en' : ''}/tag/${children}`}
       title={children}
     >
       {children}
